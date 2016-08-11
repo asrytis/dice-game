998fda837f829f876ac2005a2a2400f9f1463304
Rytis Z <asrytis@gmail.com>
2016-08-11 12:50:22 +0300

    Changing the default bg color via Router alone doesn't quite work as expected. Changed the color in native code and now it's perfect

--- client/mobile/ios/mobile/AppDelegate.m
+++ client/mobile/ios/mobile/AppDelegate.m
@@ -24,7 +24,7 @@
                                                       moduleName:@"mobile"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.17 green:0.42 blue:0.58 alpha:1.0];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];

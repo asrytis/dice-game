66d17d61814b820f57c9659704d7fecc91b3b45f
Rytis Z <asrytis@gmail.com>
2016-08-13 23:57:31 +0300

    button grays out when disabled

--- client/mobile/src/components/button.js
+++ client/mobile/src/components/button.js
@@ -3,8 +3,9 @@ import { TouchableHighlight, Text } from 'react-native';
 import { styles } from '../styles/shared';
 
 export default function Button(props) {
+	const style = [props.style || props.button, props.disabled ? styles.disabled : null];
     return (
-        <TouchableHighlight {...props} style={props.style || props.button}>
+        <TouchableHighlight {...props} style={style}>
             <Text style={props.textStyle || styles.textButton}>{props.children}</Text>
         </TouchableHighlight>
     );

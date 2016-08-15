05f551a74904763a2c5d54a364780b4f1c83b959
Rytis Z <asrytis@gmail.com>
2016-08-15 17:43:18 +0300

    progress bar animation via Animated lib

--- client/mobile/src/components/progress-bar.js
+++ client/mobile/src/components/progress-bar.js
@@ -1,5 +1,5 @@
 import React from 'React';
-import { View } from 'react-native';
+import { View, Animated, Easing } from 'react-native';
 import styles, { containerWidth } from '../styles/progress-bar';
 
 export default class ProgressBar extends React.Component {
@@ -13,38 +13,41 @@ export default class ProgressBar extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = this.getNewStateFromProps(this.props);
+        this.state = {
+            progress: new Animated.Value(0)
+        };
     }
 
     componentDidMount() {
-        this.timerId = setInterval(() => {
-            this.setState({
-                elapsed: (this.props.currentTimestamp - this.props.startTimestamp) + (new Date().getTime() - this.state.localStartTimestamp)
-            });
-        }, 50);
+        this.startAnimation();
     }
 
     componentDidReceiveProps() {
-        this.setState(this.getNewStateFromProps(this.props));
+        this.startAnimation();
     }
 
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
+    startAnimation() {
+        const progress = (this.props.currentTimestamp - this.props.startTimestamp) / this.props.duration;
+        const timeLeft = (1 - progress) * this.props.duration;
 
-    getNewStateFromProps(props) {
-        return {
-            localStartTimestamp: new Date().getTime(),
-            elapsed: props.currentTimestamp - props.startTimestamp
-        }
+        this.state.progress.setValue(progress);
+
+        Animated.timing(this.state.progress, {
+            duration: timeLeft,
+            easing: Easing.linear,
+            toValue: 1
+        }).start();
     }
 
     render() {
-        const width = containerWidth * this.state.elapsed / this.props.duration;
+        const width = this.state.progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, containerWidth]
+        });
 
         return (
             <View style={styles.container}>
-                <View style={[styles.bar, { width }]} />
+                <Animated.View style={[styles.bar, { width }]} />
             </View>
         );
     }

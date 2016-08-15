23df66ebe68dbdc05710d98f5c1564c682381c5a
Rytis Z <asrytis@gmail.com>
2016-08-16 01:05:44 +0300

    wording update

--- client/mobile/src/scenes/help.js
+++ client/mobile/src/scenes/help.js
@@ -12,7 +12,7 @@ export default class Help extends React.Component {
             <Background>
                 <Text style={styles.title}>Game rules</Text>
                 <Text style={styles.paragraph}>The goal of the game is to get the highest score in round.</Text>
-                <Text style={styles.paragraph}>The first player to roll the dice starts the round. Others will then have to roll their dice in 5 seconds before the round ends.</Text>
+                <Text style={styles.paragraph}>First player to roll the dice starts the round. Others will then have to roll their dice in 5 seconds before the round ends.</Text>
                 <Text style={styles.paragraph}>Have fun!</Text>
                 <Button style={styles.button} onPress={Actions.pop}>CLOSE</Button>
             </Background>
